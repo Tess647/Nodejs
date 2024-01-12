1349852405e10f4f02cbd1037ebd421d1e0f7c75
Tess647 <thanni_taslihah@yahoo.com>
2024-01-12 17:24:17 +0100

    upload what I've learnt so far

---
+++ 01-Nodejs-Tutorial/modules.js
@@ -0,0 +1,9 @@
+// Modules
+
+const names = require('./First-module');
+const sayHi = require('./Second-module');
+const data = require('./6-alternate-flavor')
+require('./7-mind-grenade')
+sayHi('susan')
+sayHi(names.john)
+sayHi(names.peter)
